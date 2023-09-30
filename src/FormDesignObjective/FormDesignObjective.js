4d5c3ce00f354eb41cfc333202af050a4567b835
Alan Han <hanzhujiang@gmail.com>
2023-09-30 21:56:01 +1000

    done

--- src/FormDesignObjective/FormDesignObjective.js
+++ src/FormDesignObjective/FormDesignObjective.js
@@ -11,7 +11,12 @@ const FormDesignObjective = () => {
     const [isEditing, setIsEditing] = useState(false);
     const [tempData, setTempData] = useState(initialData);
 
-    const handleChange = () => {
+    const handleChange = (e) => {
+        // e.preventdefault();
+        const {name, value}=e.target
+        console.log(name,value);
+        setTempData(prev=>({...prev,[name]:value}))
+
     };
 
     const handleEdit = () => {
@@ -19,11 +24,12 @@ const FormDesignObjective = () => {
     };
 
     const handleSave = () => {
+        setUserData(tempData)
+        setIsEditing(false)
     };
 
     const handleCancel = () => {
         setIsEditing(false)
-        
     };
 
     return (
@@ -35,12 +41,12 @@ const FormDesignObjective = () => {
                 <button onClick={handleEdit}>Edit</button>
             </div>):(<div>
                     <label name="userName">Name:</label>
-                    <input name="userName" type="text" onChange={handleChange}></input>
+                    <input name="userName" type="text" value={tempData.userName} onChange={handleChange}></input>
                     <label name="email">Email:</label>
-                    <input name="email" type="email"></input>
+                    <input name="email" type="email" value={tempData.email} onChange={handleChange}></input>
                     <label name="mobile">Mobile:</label>
-                    <input name="mobile" type="text"></input>
-                    <button onClick={handleChange}>Save</button>
+                    <input name="mobile" type="text" value={tempData.mobile} onChange={handleChange}></input>
+                    <button onClick={handleSave}>Save</button>
                     <button onClick={handleCancel}>Cancel</button>
             </div>)}
         </div>
